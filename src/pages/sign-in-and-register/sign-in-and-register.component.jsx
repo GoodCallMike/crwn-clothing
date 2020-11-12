b9b4875b6bdd10c24090b42ddc37eeb7fd283388
Michael Saylor <saylormic@gmail.com>
2020-11-12 14:11:05 -0500

    Create Sign-in form

---
+++ src/pages/sign-in-and-register/sign-in-and-register.component.jsx
@@ -0,0 +1,10 @@
+import SignIn from "../../components/sign-in/sign-in.component";
+import "./sign-in-and-register.styles.scss";
+
+const SignInAndRegister = () => (
+  <div className="sign-in-and-register">
+    <SignIn />
+  </div>
+);
+
+export default SignInAndRegister;
