e5d971e84ee0a7b50572570568fa8134e1af811c
Michael Saylor <saylormic@gmail.com>
2020-11-19 13:21:16 -0500

    create colletion pages

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import logger from "redux-logger";
+import { persistStore } from "redux-persist";
 
 import rootReducer from "./root-reducer";
 
@@ -10,4 +11,6 @@ const store = createStore(
   composeWithDevTools(applyMiddleware(...middleware))
 );
 
-export default store;
+const persistor = persistStore(store);
+
+export { store, persistor };
