9988ec9380af568f8586041d7919756020725a3f
Michael Saylor <michael_saylor@carmax.com>
2022-04-16 17:59:24 -0400

    setup directories

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-function App() {
-  return (
-    <div>
-Hello World
-    </div>
-  );
-}
+import categories from "./categories.json";
+import Directory from "./components/directory/directory.component";
+
+const App = () => {
+  return <Directory categories={categories} />;
+};
 
 export default App;
